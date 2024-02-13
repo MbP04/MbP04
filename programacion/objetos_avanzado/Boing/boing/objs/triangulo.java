e1bfd084dd520120cf4ed6ca814e06d02f73f10e
Marc Burguera <marcburpa@gmail.com>
2024-02-13 20:09:28 +0100

    Boing añadido

---
+++ programacion/objetos_avanzado/Boing/boing/objs/triangulo.java
@@ -0,0 +1,29 @@
+package boing.objs;
+import java.util.ArrayList;
+
+public class triangulo extends Objeto {
+
+    public triangulo(Posicion pos, int dirx, int diry) {
+
+        super(pos, dirx, diry);
+
+    }
+
+    public ArrayList<Posicion> forma() {
+
+        ArrayList<Posicion> forma = new ArrayList<Posicion>();
+
+        forma.add(new Posicion(2, 0));
+        forma.add(new Posicion(1, 1));
+        forma.add(new Posicion(2, 1));
+        forma.add(new Posicion(3, 1));
+        forma.add(new Posicion(0, 2));
+        forma.add(new Posicion(1, 2));
+        forma.add(new Posicion(2, 2));
+        forma.add(new Posicion(3, 2));
+        forma.add(new Posicion(4, 2));
+        return forma;
+
+    }
+
+}
